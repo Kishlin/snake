dfce6535d1f27aca1f1e7858494a7c837184bb02
Kishlin <hello@pierrelouislegrand.com>
2025-02-09 21:13:57 +0100

    Hotfix: Update build_windows.sh

--- build_windows.sh
+++ build_windows.sh
@@ -5,7 +5,6 @@ env SDL2_DIR="/usr/local/x86_64-w64-mingw32" \
     CGO_ENABLED="1" \
     CC="/usr/bin/x86_64-w64-mingw32-gcc" \
     GOOS="windows" \
-    CGO_LDFLAGS="-L/usr/local/x86_64-w64-mingw32/lib -lmingw32 -lSDL2 -lSDL2_ttf -mwindows"
+    CGO_LDFLAGS="-L/usr/local/x86_64-w64-mingw32/lib -lmingw32 -lSDL2 -L/usr/local/x86_64-w64-mingw32/lib -LSDL2_ttf" \
     CGO_CFLAGS="-D_REENTRANT" \
-    go build -x -o out/snake.exe bin/main.go
-
+    go build -x -ldflags "-H windowsgui" -o out/snake.exe bin/main.go
