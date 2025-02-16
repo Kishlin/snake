41dfbd5581414cbd786704f8ad3581a5b5aa1fac
Pierre-Louis Legrand <plegrand@synthesio.com>
2025-02-16 01:47:32 +0100

    Hotfix: Prevent the console to show up on Windows

--- build_windows.i686.sh
+++ build_windows.i686.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-CGO_ENABLED=1 CC=i686-w64-mingw32-gcc GOOS=windows GOARCH=386 go build -ldflags "-s -w" -o out/snake.i686.exe bin/main.go
+CGO_ENABLED=1 \
+  CC=i686-w64-mingw32-gcc \
+  GOOS=windows \
+  GOARCH=386 \
+  go build -ldflags "-s -w -H windowsgui" \
+  -o out/snake.i686.exe \
+  bin/main.go
--- build_windows.x86_64.sh
+++ build_windows.x86_64.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-CGO_ENABLED=1 CC=x86_64-w64-mingw32-gcc GOOS=windows GOARCH=amd64 go build -ldflags "-s -w" -o out/snake.x86_64.exe bin/main.go
+CGO_ENABLED=1 \
+  CC=x86_64-w64-mingw32-gcc \
+  GOOS=windows \
+  GOARCH=amd64 \
+  go build -ldflags "-s -w -H windowsgui" \
+  -o out/snake.x86_64.exe \
+  bin/main.go
